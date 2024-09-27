b079f475bf229aede8a64578d544676c1530ed55
flavia-dantas <flavia.dantas2@hotmail.com>
2024-09-27 19:11:08 -0300

    feat: user registration using prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -11,6 +11,7 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
@@ -46,8 +47,15 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  function handleRegister(data: RegisterFormData) {
-    console.log(data)
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
